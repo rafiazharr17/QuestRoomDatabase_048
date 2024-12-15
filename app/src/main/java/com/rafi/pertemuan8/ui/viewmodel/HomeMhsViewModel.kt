6137294a01e907b74f0bbcded616a5966d6ac264
rafiazharr17 <rafipilo17@gmail.com>
2024-12-15 21:09:11 +0700

    membuat class HomeMhsViewModel untuk mengelola state UI

--- app/src/main/java/com/rafi/pertemuan8/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/rafi/pertemuan8/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,7 +1,50 @@
 package com.rafi.pertemuan8.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.rafi.pertemuan8.data.entity.Mahasiswa
+import com.rafi.pertemuan8.repository.RepositoryMhs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
+class HomeMhsViewModel (
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+    val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message?: "Terjadi kesalah"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true
+            )
+        )
+}
 
 data class HomeUiState(
     val listMhs: List<Mahasiswa> = listOf(),
