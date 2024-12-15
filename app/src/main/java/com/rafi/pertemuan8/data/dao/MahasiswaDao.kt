5b5a2453dfda1fd78d9a51babf6e475cb5a8cf1d
rafiazharr17 <rafipilo17@gmail.com>
2024-12-15 17:24:45 +0700

    untuk menghapus data mahasiswa tertentu dari database

--- app/src/main/java/com/rafi/pertemuan8/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/rafi/pertemuan8/data/dao/MahasiswaDao.kt
@@ -1,6 +1,7 @@
 package com.rafi.pertemuan8.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.rafi.pertemuan8.data.entity.Mahasiswa
@@ -16,4 +17,7 @@ interface MahasiswaDao {
 
     @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
     fun getMahasiswa(nim : String): Flow<Mahasiswa>
+
+    @Delete
+    suspend fun deleteMahasiswa (mahasiswa: Mahasiswa)
 }
