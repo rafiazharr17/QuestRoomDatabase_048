250d2af58a2aa6335abb8d855f4219480ae39f88
rafiazharr17 <rafipilo17@gmail.com>
2024-12-15 21:14:20 +0700

    membuat data class DetailUiState untuk menampung data yang akan ditampilkan di UI

---
+++ app/src/main/java/com/rafi/pertemuan8/ui/viewmodel/DetailMhsViewModel.kt
@@ -0,0 +1,16 @@
+package com.rafi.pertemuan8.ui.viewmodel
+
+
+
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
