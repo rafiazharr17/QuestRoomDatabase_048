f8035dec7794164d7fbab273095eaa61fcbe0bee
rafiazharr17 <rafipilo17@gmail.com>
2024-12-15 17:20:15 +0700

    untuk mengambil semua data mahasiswa yang tersimpan di database

--- app/src/main/java/com/rafi/pertemuan8/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/rafi/pertemuan8/data/dao/MahasiswaDao.kt
@@ -2,10 +2,15 @@ package com.rafi.pertemuan8.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.rafi.pertemuan8.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
 }
